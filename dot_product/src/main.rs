8bef62d4548e00a6cda6ebf4100fde7090c9011d
McPotato <luc.sydney.georges@gmail.com>
2021-05-01 12:27:48 +0200

    dot product for neuron

--- dot_product/src/main.rs
+++ dot_product/src/main.rs
@@ -1,3 +1,22 @@
+fn neuron(inputs: &[f64], weights: &[f64], bias: f64) -> f64 {
+    inputs
+        .iter()
+        .zip(weights.iter())
+        .map(|(x, y)| x * y)
+        .sum::<f64>()
+        + bias
+}
+
 fn main() {
-    println!("Hello, world!");
+    let inputs: Vec<f64> = vec![1., 2., 3., 2.5];
+
+    let weights: Vec<&[f64]> = vec![
+        &[0.2, 0.8, -0.5, 1.],
+        &[0.5, -0.91, 0.26, -0.5],
+        &[-0.26, -0.27, 0.17, 0.87],
+    ];
+
+    let biases: Vec<f64> = vec![2.0, 3.0, 0.5];
+
+    println!("{}", neuron(&inputs, weights[0], biases[0]));
 }
